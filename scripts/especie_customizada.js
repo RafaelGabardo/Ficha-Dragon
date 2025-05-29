875b8ba9b8b1c47e698cd980d06cdb0eeaae8564
Rafael Gabardo <rggabardo@gmail.com>
2025-05-29 19:43:42 -0300

    Ajustando o javascript de espécie customizada e adicionando o input submit nos formulários de criação

--- scripts/especie_customizada.js
+++ scripts/especie_customizada.js
@@ -1,8 +1,19 @@
 let especie_customizada = document.getElementById('especie-customizada');
 let checkbox = document.getElementById('checkbox');
+let select_especie = document.getElementById('especie');
 
 checkbox.onclick = function() {
   if (checkbox.checked) {
     especie_customizada.disabled = false;
+    especie_customizada.style.backgroundColor = 'white';
+
+    select_especie.disabled = true;
+    select_especie.style.backgroundColor = '#D4D4D4';
+  } else {
+    especie_customizada.disabled = true;
+    especie_customizada.style.backgroundColor = '#D4D4D4';
+
+    select_especie.disabled = false;
+    select_especie.style.backgroundColor = 'white';
   }
 }

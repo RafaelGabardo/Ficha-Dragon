d8a19da5252db188e1dcb3255af8bcad93da5efe
Rafael Gabardo <rggabardo@gmail.com>
2025-06-17 09:18:50 -0300

    Ajustando o script da pagina dados.html

--- scripts/dados.js
+++ scripts/dados.js
@@ -1,5 +1,7 @@
+// Declrando variáveis para os elementos HTML
 let quantidade = document.getElementById('quantidade');
 let resultado_dado = document.getElementById('resultado-dado');
+let resultado_cada_dado = document.getElementById('resultado-cada-dado');
 let d4 = document.getElementById('d4');
 let d6 = document.getElementById('d6');
 let d8 = document.getElementById('d8');
@@ -9,6 +11,7 @@ let d20 = document.getElementById('d20');
 let d100 = document.getElementById('d100');
 
 function rolar_dado(numero_de_lados) {
+  let resultados = [];
   let quantidade_rolagens = parseInt(quantidade.value);
 
   if (isNaN(quantidade_rolagens) || quantidade_rolagens <= 0) {
@@ -20,7 +23,12 @@ function rolar_dado(numero_de_lados) {
 
   for (let i = 0; i < quantidade_rolagens; i++) {
     let numero_aleatorio = Math.floor(Math.random() * numero_de_lados) + 1;
+    resultados.push(numero_aleatorio);
     soma_resultados += numero_aleatorio;
+  }
+
+  if (quantidade_rolagens > 1) {
+    resultado_cada_dado.textContent = 'Resultados: ' + resultados.join(', ');
   }
 
   resultado_dado.textContent = soma_resultados;
